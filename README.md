edab46b80dbc21112485174289c82b9c52e0a1c7
Jonathan Stevens <jonathanstevens89@gmail.com>
2019-03-27 10:03:58 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Homebrew tools
+
+A Homebrew repo to install internal tools
+
+Make sure `HOMEBREW_GITHUB_API_TOKEN` is set:  
+Either prefix it with the `brew install` command or `export` it to your environment.  
+Then:
+
+```
+brew tap weave-lab/homebrew-tools
+  
+brew install bart
+```
---
+++ formula/bart.rb
@@ -0,0 +1,13 @@
+class Bart < Formula
+  desc "bart cli"
+  homepage "https://github.com/weave-lab/monorail"
+  url "../bart-1.0.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "b4aa573718ba1fc55c4950358970eabf5b1183628e16eaf9b810568faf74b15a"
+  head "https://github.com/weave-lab/monorail.git"
+
+  def install
+    # install bart script, create symlink to script in /usr/local/bin
+    bin.install "bart"
+  end
+
+end
