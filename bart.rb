97c98b3aa5c18184e4025a4b7121ee63eb85ee38
Jonathan Stevens <jonathanstevens89@gmail.com>
2019-03-27 10:24:17 -0600

    update version

--- bart.rb
+++ bart.rb
@@ -4,6 +4,7 @@ class Bart < Formula
   url "./bart-1.0.tar.gz"
   sha256 "b4aa573718ba1fc55c4950358970eabf5b1183628e16eaf9b810568faf74b15a"
   head "https://github.com/weave-lab/bart.git"
+  version "v0.0.1"
 
   def install
     # install bart script, create symlink to script in /usr/local/bin
