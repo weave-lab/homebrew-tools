4a30b0110219dcfc5f54a830efa4881d4e521b16
Jonathan Stevens <jonathanstevens89@gmail.com>
2019-03-28 07:15:02 -0600

    updating sha

--- formula/bart.rb
+++ formula/bart.rb
@@ -3,8 +3,8 @@ require "formula"
 class Bart < Formula
   desc "bart cli"
   homepage "https://github.com/weave-lab/bart"
-  url "https://github.com/weave-lab/bart/releases/download/v0.0.1/bart_0.0.1_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "72de295df9a0e4f70c78a3133dc485d0e6e90abda7343cb9835bb076dcc2b573"
+  url "https://github.com/weave-lab/bart/releases/download/v0.0.1/bart_0.0.1_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "d2d771c98cc955da3c3caca27fdd61701402fae396ac05561955064084d9d04f"
   head "https://github.com/weave-lab/bart.git"
   version "0.0.1"
 
