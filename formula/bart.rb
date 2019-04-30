3e6e5d1c9db48f41945c99bf749fe80cd425e9d6
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-30 15:34:34 -0600

    Brew formula update for bart version v1.0.82

--- formula/bart.rb
+++ formula/bart.rb
@@ -3,9 +3,9 @@ require_relative "../download_strategy"
 class Bart < Formula
   desc ""
   homepage "https://github.com/weave-lab/bart"
-  url "https://github.com/weave-lab/bart/releases/download/v1.0.80/bart_1.0.80_Darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "1.0.80"
-  sha256 "505234db39082e79c51d77d4c320d4f746f24b83952318b995ade808ff1eaafd"
+  url "https://github.com/weave-lab/bart/releases/download/v1.0.82/bart_1.0.82_Darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "1.0.82"
+  sha256 "46c9a4ba376ab5d527cf196a6863ee46ebb88b8f9c5cdbad826b4bbe7db2faef"
 
   def install
     bin.install "bart"
