9a3e152dbc9e74086d2d0903f4b28052645c3164
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-03 12:25:28 -0600

    Brew formula update for bart version v1.1.797

--- formula/bart.rb
+++ formula/bart.rb
@@ -3,16 +3,16 @@ require_relative "../download_strategy"
 class Bart < Formula
   desc ""
   homepage "https://github.com/weave-lab/bart"
-  version "1.1.795"
+  version "1.1.797"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/weave-lab/bart/releases/download/v1.1.795/bart_1.1.795_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "b4f4710f81af1153c184c5e546c7e43b699ef7750133a8893f1524350ddaeb66"
+    url "https://github.com/weave-lab/bart/releases/download/v1.1.797/bart_1.1.797_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "d5d8f1c505df4ec6d935b48ba5314b93cee29867752ba087e3b819a71c34b5a6"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/weave-lab/bart/releases/download/v1.1.795/bart_1.1.795_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "e8b843afba4e00e23b56f2440259f86746bfca0d122b8e42c02cd2a765cec6ba"
+      url "https://github.com/weave-lab/bart/releases/download/v1.1.797/bart_1.1.797_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "ecdb32056814be2f1f57ac4ac42a0292f789c014ba8f3355b73854e024ee57ea"
     end
   end
 
