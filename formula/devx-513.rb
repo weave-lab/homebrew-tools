05b16da5b67c1066ac3dbc9b155022d1ade75225
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-22 10:25:47 -0600

    Brew formula update for devx-513 version v0.0.6

--- formula/devx-513.rb
+++ formula/devx-513.rb
@@ -3,16 +3,16 @@ require_relative "../download_strategy"
 class Devx513 < Formula
   desc ""
   homepage "https://github.com/weave-lab/devx-513"
-  version "0.0.2"
+  version "0.0.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/weave-lab/devx-513/releases/download/v0.0.2/devx-513_0.0.2_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "afe04fc925175d23ad27f1fcc0cffa52507daa20b5907d2b2a10577fda8b16b7"
+    url "https://github.com/weave-lab/devx-513/releases/download/v0.0.6/devx-513_0.0.6_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "52ec390a1d58eb51376dac19a813e44c2658c6890bb0d71263c71302059ad71a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/weave-lab/devx-513/releases/download/v0.0.2/devx-513_0.0.2_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "9e0f2d49131c18e7e2be56694a4b21e5acfdaf442dc0407d9d1d6c2c59cc77e4"
+      url "https://github.com/weave-lab/devx-513/releases/download/v0.0.6/devx-513_0.0.6_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "ebedd51ef9fdf146933217ea9c46595c9bda95d507d1107edf8262fbd893924d"
     end
   end
 
