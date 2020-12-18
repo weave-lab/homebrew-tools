47af6f12bedced51dae1a8dc0e2811ab53fc9b9d
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-18 09:46:41 -0700

    Brew formula update for analytics-etl version v0.1.15

--- formula/analytics.rb
+++ formula/analytics.rb
@@ -3,16 +3,16 @@ require_relative "../download_strategy"
 class Analytics < Formula
   desc ""
   homepage "https://github.com/weave-lab/analytics-etl"
-  version "0.1.14"
+  version "0.1.15"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/weave-lab/analytics-etl/releases/download/v0.1.14/analytics-etl_0.1.14_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "998b0968967eada8125f26ae3fa465e18a430e10ba4bef2a71528e25561c65d1"
+    url "https://github.com/weave-lab/analytics-etl/releases/download/v0.1.15/analytics-etl_0.1.15_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "36b00e6a9aa1658591b7cb8e5987678cfd3aa28769ce9d971fb1ee946a3e05cb"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/weave-lab/analytics-etl/releases/download/v0.1.14/analytics-etl_0.1.14_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "6a0d23f6cd75ecd5bc01a0e6e9b07befd31a32dc4149e0b682a831ed8ceca914"
+      url "https://github.com/weave-lab/analytics-etl/releases/download/v0.1.15/analytics-etl_0.1.15_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "a0a518482bed1f3d01f7513161de752fb899ad75673cca4f1ba530c5530372db"
     end
   end
 
